02fc4e7e4525572d1254dc631d70a43194065d69
ifeanyi-valentine <urielifeanyi@gmail.com>
2025-09-24 08:32:06 +0100

    chore: remove example code

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,3 @@ pub use send::{
     writable::{WritableSink, WritableStream, WritableStreamDefaultController},
 };
 pub use streams::{send::*, *};
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
