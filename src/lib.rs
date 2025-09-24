cf5d772db977aa4e80d3c9d7fd7fac15e940fda1
ifeanyi-valentine <urielifeanyi@gmail.com>
2025-09-24 08:31:28 +0100

    feat: export common types at top level

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 pub mod streams;
+pub use send::{
+    readable::{
+        ReadableByteSource, ReadableSource, ReadableStream, ReadableStreamDefaultController,
+    },
+    transform::{
+        IdentityTransformer, TransformStream, TransformStreamDefaultController, Transformer,
+    },
+    writable::{WritableSink, WritableStream, WritableStreamDefaultController},
+};
 pub use streams::{send::*, *};
 
 pub fn add(left: u64, right: u64) -> u64 {
